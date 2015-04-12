119fbcb0d2cda348a4958ac38adebd944134e34d
cornernote <cornernote@gmail.com>
2015-04-12 15:50:48 +0930

    updates with Brett and Frazer

--- js/agency.js
+++ js/agency.js
@@ -5,8 +5,8 @@
  */
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
+$(function () {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
@@ -18,20 +18,25 @@ $(function() {
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
-})
+});
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+$('.navbar-collapse ul li a').click(function () {
     $('.navbar-toggle:visible').click();
 });
 
-$('div.modal').on('show.bs.modal', function() {
-	var modal = this;
-	var hash = modal.id;
-	window.location.hash = hash;
-	window.onhashchange = function() {
-		if (!location.hash){
-			$(modal).modal('hide');
-		}
-	}
+$('div.modal').on('show.bs.modal', function () {
+    var modal = this;
+    window.location.hash = modal.id;
+    window.onhashchange = function () {
+        if (!location.hash) {
+            $(modal).modal('hide');
+        }
+    }
+});
+
+// service tabs
+$('#service-tabs').find('a').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
 });
